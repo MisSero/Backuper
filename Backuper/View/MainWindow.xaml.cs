3551bc6a4179d2d316a16887bdaf6dd4527567f2
MisSero <misha555835@gmail.com>
2023-08-30 14:36:38 +0300

    design app as working in tray, add icon

--- Backuper/View/MainWindow.xaml.cs
+++ Backuper/View/MainWindow.xaml.cs
@@ -1,25 +1,54 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Forms;
+using Backuper.Model;
 
 namespace Backuper
 {
     public partial class MainWindow : Window
     {
+        private SaveModel saver;
         public MainWindow()
         {
+            saver = new SaveModel();
+            BackupModel backup = new(saver.DataKeeper.SourceDirectory, 
+                saver.DataKeeper.TargetDirectory);
+
             InitializeComponent();
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            Hide();
+
+            var notifyIcon = new NotifyIcon();
+            notifyIcon.Icon = new System.Drawing.Icon("Resources\\icon.ico");
+            notifyIcon.Visible = true;
+
+            notifyIcon.DoubleClick += (s, args) =>
+            {
+                Show();
+                WindowState = WindowState.Normal;
+            };
+
+            notifyIcon.ContextMenuStrip = new ContextMenuStrip();
+            notifyIcon.ContextMenuStrip.Items.Add("Открыть настройки", null, (s, args) =>
+            {
+                Show();
+                WindowState = WindowState.Normal;
+            });
+            notifyIcon.ContextMenuStrip.Items.Add("Закрыть приложение", null, (s, args) =>
+            {
+                notifyIcon.Dispose();
+                saver.Save();
+                System.Windows.Application.Current.Shutdown();
+            });
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;
+            Hide();
         }
     }
 }
