b0c35d6fb890dc12b1946f8b191801dbf19437a9
MisSero <misha555835@gmail.com>
2023-08-30 16:33:41 +0300

    add possibility to make backups as desired

--- Backuper/View/MainWindow.xaml.cs
+++ Backuper/View/MainWindow.xaml.cs
@@ -4,88 +4,96 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using Backuper.Model;
 
-namespace Backuper
+namespace Backuper;
+
+public partial class MainWindow : Window
 {
-    public partial class MainWindow : Window
+    private SaveModel saver;
+    public MainWindow()
     {
-        private SaveModel saver;
-        public MainWindow()
+        saver = new SaveModel();
+        BackupModel.Backup(saver.DataKeeper.SourceDirectory, 
+            saver.DataKeeper.TargetDirectory);
+
+        InitializeComponent();
+
+        // Создаём привязки к textBox
+        Binding binding = new Binding();
+        binding.Source = saver;
+        binding.Mode = BindingMode.TwoWay;
+        binding.Path = new PropertyPath("DataKeeper.SourceDirectory");
+        binding.UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged;
+        SourceTextBox.SetBinding(TextBox.TextProperty, binding);
+
+        binding = new Binding();
+        binding.Source = saver;
+        binding.Mode = BindingMode.TwoWay;
+        binding.Path = new PropertyPath("DataKeeper.TargetDirectory");
+        binding.UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged;
+        TargetTextBox.SetBinding(TextBox.TextProperty, binding);
+
+    }
+
+    // Собитие при запуске окна, обеспечивает работу приложения в фоне
+    private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+    {
+        Hide();
+
+        var notifyIcon = new System.Windows.Forms.NotifyIcon();
+        notifyIcon.Icon = new System.Drawing.Icon("Resources\\icon.ico");
+        notifyIcon.Visible = true;
+
+        notifyIcon.DoubleClick += (s, args) =>
         {
-            saver = new SaveModel();
-            BackupModel.Backup(saver.DataKeeper.SourceDirectory, 
-                saver.DataKeeper.TargetDirectory);
+            Show();
+            WindowState = WindowState.Normal;
+        };
 
-            InitializeComponent();
-
-            // Создаём привязки к textBox
-            Binding binding = new Binding();
-            binding.Source = saver;
-            binding.Mode = BindingMode.TwoWay;
-            binding.Path = new PropertyPath("DataKeeper.SourceDirectory");
-            binding.UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged;
-            SourceTextBox.SetBinding(TextBox.TextProperty, binding);
-
-            binding = new Binding();
-            binding.Source = saver;
-            binding.Mode = BindingMode.TwoWay;
-            binding.Path = new PropertyPath("DataKeeper.TargetDirectory");
-            binding.UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged;
-            TargetTextBox.SetBinding(TextBox.TextProperty, binding);
-
-        }
-
-        // Собитие при запуске окна, обеспечивает работу приложения в фоне
-        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        notifyIcon.ContextMenuStrip = new System.Windows.Forms.ContextMenuStrip();
+        notifyIcon.ContextMenuStrip.Items.Add("Сделать резервное копирование", null, (s, args) =>
         {
-            Hide();
-
-            var notifyIcon = new System.Windows.Forms.NotifyIcon();
-            notifyIcon.Icon = new System.Drawing.Icon("Resources\\icon.ico");
-            notifyIcon.Visible = true;
-
-            notifyIcon.DoubleClick += (s, args) =>
-            {
-                Show();
-                WindowState = WindowState.Normal;
-            };
-
-            notifyIcon.ContextMenuStrip = new System.Windows.Forms.ContextMenuStrip();
-            notifyIcon.ContextMenuStrip.Items.Add("Открыть настройки", null, (s, args) =>
-            {
-                Show();
-                WindowState = WindowState.Normal;
-            });
-            notifyIcon.ContextMenuStrip.Items.Add("Закрыть приложение", null, (s, args) =>
-            {
-                notifyIcon.Dispose();
-                saver.Save();
-                System.Windows.Application.Current.Shutdown();
-            });
-        }
-
-        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+            BackupModel.Backup(SourceTextBox.Text, TargetTextBox.Text);
+        });
+        notifyIcon.ContextMenuStrip.Items.Add("Открыть настройки", null, (s, args) =>
         {
-            e.Cancel = true;
-            Hide();
-        }
-
-        // Обработкич события по клику кнопки для выбора пути
-        private void SelectPath_Click(object sender, RoutedEventArgs e)
+            Show();
+            WindowState = WindowState.Normal;
+        });
+        notifyIcon.ContextMenuStrip.Items.Add("Закрыть приложение", null, (s, args) =>
         {
-            Button button = (Button)sender;
-            string textBoxName = button.Tag.ToString();
+            notifyIcon.Dispose();
+            saver.Save();
+            System.Windows.Application.Current.Shutdown();
+        });
+    }
 
-            using (var dialog = new System.Windows.Forms.FolderBrowserDialog())
+    private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+    {
+        e.Cancel = true;
+        Hide();
+    }
+
+    // Обработкич события по клику кнопки для выбора пути
+    private void SelectPath_Click(object sender, RoutedEventArgs e)
+    {
+        Button button = (Button)sender;
+        string textBoxName = button.Tag.ToString();
+
+        using (var dialog = new System.Windows.Forms.FolderBrowserDialog())
+        {
+            System.Windows.Forms.DialogResult result = dialog.ShowDialog();
+            if (result == System.Windows.Forms.DialogResult.OK)
             {
-                System.Windows.Forms.DialogResult result = dialog.ShowDialog();
-                if (result == System.Windows.Forms.DialogResult.OK)
-                {
-                    if (textBoxName == "SourceTextBox")
-                        SourceTextBox.Text = dialog.SelectedPath;
-                    else if (textBoxName == "TargetTextBox")
-                        TargetTextBox.Text = dialog.SelectedPath;
-                }
+                if (textBoxName == "SourceTextBox")
+                    SourceTextBox.Text = dialog.SelectedPath;
+                else if (textBoxName == "TargetTextBox")
+                    TargetTextBox.Text = dialog.SelectedPath;
             }
         }
+    }
+
+    private void Backup_Click(object sender, RoutedEventArgs e)
+    {
+        BackupModel.Backup(SourceTextBox.Text, TargetTextBox.Text);
     }
 }
